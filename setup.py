7272733ca998149a7197df467964a8221a55bbf6
Christo De Lange <111363591+christokur@users.noreply.github.com>
2022-12-30 11:44:36 -0500

    Update setup.py
    Version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/milin/gitown',
-    version='0.1.1',
+    version='0.1.8',
     entry_points={
         'console_scripts': [
             'gitown = gitown.gitown:main',
